f725ea3ba95100a8d850145a25f190b802a8cf94
Francesco Peluso <francesco.peluso@agtweb.it>
2025-12-20 16:56:03 +0100

    add_cliente_service_ricerca_per_chiave

--- RAAF-GAMING-NEW/app/Services/Profilo/ClienteService.php
+++ RAAF-GAMING-NEW/app/Services/Profilo/ClienteService.php
@@ -2,6 +2,9 @@
 
 namespace App\Services\Profilo;
 
+use App\Models\Profilo\Cliente;
+use Illuminate\Support\Facades\Session;
+
 class ClienteService
 {
     /**
@@ -11,4 +14,30 @@ class ClienteService
     {
         //
     }
+
+    /**
+     * Cerca un cliente basandosi sulla chiave fornita.
+     *
+     * @param string $id.
+     * @return \App\Models\Profilo\Cliente Il modello Cliente trovato.
+     * @throws \InvalidArgumentException Se l'id fornito è vuoto o non valido.
+     */
+    public function ricercaPerChiave($id)
+    {
+        if($id == null || $id == "")
+			throw new \InvalidArgumentException("Inserito un id null o vuoto");
+
+        $cliente = Session::get('Cliente');
+
+        if(isset($cliente))
+            return $cliente;
+
+        $cliente = Cliente::where('email',$id);
+
+        Session::put('Cliente',$cliente);
+
+        return $cliente;
+    }
+
+    
 }
