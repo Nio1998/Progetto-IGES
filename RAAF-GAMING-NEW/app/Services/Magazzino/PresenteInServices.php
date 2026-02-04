bb9a05ee66592531eb540ba96d20e958a356086b
Francesco Peluso <francesco.peluso@agtweb.it>
2026-02-04 15:01:32 +0100

    add_migrazione_ricercaPerChiave_presentein

--- RAAF-GAMING-NEW/app/Services/Magazzino/PresenteInServices.php
+++ RAAF-GAMING-NEW/app/Services/Magazzino/PresenteInServices.php
@@ -2,6 +2,8 @@
 
 namespace App\Services\Magazzino;
 
+use App\Models\Magazzino\PresenteIn;
+
 class PresenteInServices
 {
     /**
@@ -10,5 +12,26 @@ class PresenteInServices
     public function __construct()
     {
         //
+    }
+
+
+    /**
+     * Cerca un record PresenteIn basandosi sulla chiave composta fornita.
+     *
+     * @param int $id1 L'ID del prodotto.
+     * @param string|null $id2 Il codice del magazzino.
+     * @return \App\Models\PresenteIn|null Il modello PresenteIn trovato o null.
+     * @throws \InvalidArgumentException Se i parametri forniti non sono validi.
+     */
+    public function ricercaPerChiave(int $id1, ?string $id2): ?PresenteIn
+    {
+        if ($id1 < 0 || $id2 == null || $id2 == "")
+            throw new \InvalidArgumentException("id1 negativo e/o id2 è null o id2 è stringa vuota");
+        
+        $presenteIn = PresenteIn::where('prodotto', $id1)
+                                ->where('magazzino', $id2)
+                                ->first();
+        
+        return $presenteIn;
     }
 }
