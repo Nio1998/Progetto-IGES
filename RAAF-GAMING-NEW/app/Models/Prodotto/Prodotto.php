3ef08e9f4d75da64779564821929a1b473d0f7cc
Antonio De Lucia <antoniodelucia@MacBook-Pro-di-Antonio-2.local>
2026-02-04 16:35:58 +0100

    preparazione presenteIn

--- RAAF-GAMING-NEW/app/Models/Prodotto/Prodotto.php
+++ RAAF-GAMING-NEW/app/Models/Prodotto/Prodotto.php
@@ -1,10 +1,68 @@
 <?php
 
-namespace App\Models\Prodotto;
+namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Magazzino\PresenteIn;
 
 class Prodotto extends Model
 {
-    //
+    use HasFactory;
+
+    /**
+     * La tabella associata al model.
+     *
+     * @var string
+     */
+    protected $table = 'prodotto';
+
+    /**
+     * La chiave primaria della tabella.
+     *
+     * @var string
+     */
+    protected $primaryKey = 'codice_prodotto';
+
+    /**
+     * Indica se il model dovrebbe avere i timestamp automatici.
+     *
+     * @var bool
+     */
+    public $timestamps = true;
+
+    /**
+     * Gli attributi assegnabili in massa.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'prezzo',
+        'copertina',
+        'sconto',
+        'data_uscita',
+        'nome',
+        'quantita_fornitura',
+        'ultima_fornitura',
+        'fornitore',
+        'gestore',
+    ];
+
+    /**
+     * Gli attributi che devono essere castati a tipi nativi.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'prezzo' => 'double',
+        'sconto' => 'decimal:0',
+        'data_uscita' => 'date',
+        'quantita_fornitura' => 'integer',
+        'ultima_fornitura' => 'date',
+    ];
+
+    public function presenteIn()
+    {
+        return $this->hasMany(PresenteIn::class, 'magazzino', 'indirizzo');
+    }
 }
