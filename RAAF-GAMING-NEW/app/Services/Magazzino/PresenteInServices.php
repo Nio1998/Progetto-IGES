55ea5b80326f7372006f5e85f2ccfff084f34ab2
Francesco Peluso <francesco.peluso@agtweb.it>
2026-02-04 14:58:08 +0100

    add_PresenteInServices

---
+++ RAAF-GAMING-NEW/app/Services/Magazzino/PresenteInServices.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Services\Magazzino;
+
+class PresenteInServices
+{
+    /**
+     * Create a new class instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+}
