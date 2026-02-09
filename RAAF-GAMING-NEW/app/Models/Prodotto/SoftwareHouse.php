2be40fa9ff513ca3f3790a58fcea4a77eb927036
Antonio Maddaloni <madda9818@gmail.com>
2026-02-09 16:54:54 +0100

    add_prodotto_model

---
+++ RAAF-GAMING-NEW/app/Models/Prodotto/SoftwareHouse.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models\Prodotto;
+
+use App\Models\Prodotto\Videogioco;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class SoftwareHouse extends Model
+{
+    /** @use HasFactory<\Database\Factories\SoftwareHouseFactory> */
+    use HasFactory;
+
+    public $timestamps = false;
+
+    protected $table = 'softwarehouse';
+
+    protected $primaryKey = 'nomesfh';
+    protected $keyType = 'string';
+    public $incrementing = false;
+
+    protected $fillable = [
+        'nomesfh',
+        'logo',
+    ];
+
+    // Relationship
+
+    public function videogiochi()
+    {
+        return $this->hasMany(Videogioco::class, 'software_house', 'nomesfh');
+    }
+}
