43b3cb93345e52e0c36c9487c1acd5c512eea802
Antonio De Lucia <antoniodelucia@Mac.station>
2026-02-19 17:28:02 +0100

    conclusi i test per recensisce

---
+++ RAAF-GAMING-NEW/tests/resources/expected/RecensiscePublicaNuovaRecensione.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    ['cliente' => 'f.peluso25@gmail.com',         'prodotto' => 2, 'voto' => 10, 'commento' => 'bellissmo'],
+    ['cliente' => 'antoniomaddaloni@hotmail.com',  'prodotto' => 2, 'voto' => 7,  'commento' => 'vero veramente'],
+    ['cliente' => 'f.peluso25@gmail.com',          'prodotto' => 1, 'voto' => 10, 'commento' => 'bello'],
+];
---
+++ RAAF-GAMING-NEW/tests/resources/expected/RecensiscePublicaRecensioneAsc.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    ['cliente' => 'antoniomaddaloni@hotmail.com',  'prodotto' => 2, 'voto' => 7,  'commento' => 'vero veramente'],
+    ['cliente' => 'f.peluso25@gmail.com',         'prodotto' => 2, 'voto' => 10, 'commento' => 'bellissmo'],
+];
